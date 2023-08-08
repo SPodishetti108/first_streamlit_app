36207059f7fdce699999c2eb2ff32ca625029652
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-08 15:35:12 +0530

    Update streamlit_app.py
    streamlit functions tested

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,25 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('idli \n Dosa \n Wada \n  :)')
+st.header('Breakfast Menu')
+st.subheader("South Indian : ")
+st.text('idli \n Dosa \n Wada \n  :)')
+
+st.subheader("North Indian : ")
+st.text('Wada Paw \n Samosa \n Paw Baji \n  :)')
+
+# Other Streamlist functions
+st.write("Hello, Streamlit!")
+st.write(12345)
+st.write(["apple", "banana", "orange"])
+
+st.text("This is some text.")
+
+st.markdown("## This is a Markdown Heading")
+st.markdown("This is **bold** and *italic* text.")
+
+
+
+
 #print("Gam Ganesha")
