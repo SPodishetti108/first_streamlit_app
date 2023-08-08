def6bdc4a3097a8f3b5a6dc9b89151d2634ac614
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-08 15:55:36 +0530

    Update streamlit_app.py
    fix err

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 st.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
 
 """
 
