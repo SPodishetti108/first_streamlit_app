0015fc9e04d09db7e2ed33a9492a278398499537
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-10 02:41:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 import requests as req
 
+
 st.title('My Mom''s New Healthy Diner')
 st.header('Breakfast Menu')
 fruityvice_responce = req.get("https://fruityvice.com/api/fruit/watermelon")
