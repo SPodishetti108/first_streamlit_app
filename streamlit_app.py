77ba0d4414a811128f7d8fbadeac60481cee6e89
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-10 02:37:02 +0530

    Update streamlit_app.py
    err

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
-import requests req
+import requests as req
 
 st.title('My Mom''s New Healthy Diner')
 st.header('Breakfast Menu')
