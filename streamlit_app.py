256753b6769b2ac68caac75c31503e2a8937799c
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-10 02:45:42 +0530

    Update streamlit_app.py
    err

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
-import requests as req
+import requests
 
 
 st.title('My Mom''s New Healthy Diner')
 st.header('Breakfast Menu')
-fruityvice_responce = req.get("https://fruityvice.com/api/fruit/watermelon") 
-st.text(fruityvice_responce)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
