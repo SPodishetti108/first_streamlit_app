edd4d91b1a0958e76a40d1adaab0d682fe458db4
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-10 02:44:32 +0530

    Update streamlit_app.py
    err

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,25 +8,3 @@ st.title('My Mom''s New Healthy Diner')
 st.header('Breakfast Menu')
 fruityvice_responce = req.get("https://fruityvice.com/api/fruit/watermelon") 
 st.text(fruityvice_responce)
-
-'''
-st.title('My Parents New Healthy Diner')
-
-st.header('Breakfast Menu')
-st.text('🍞Omega 3 & Blueberry Oatmeal')
-st.text('🥗Kale, Spinach & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑🍞Avocado Toast')
-
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_to_show  = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
-#st.dataframe(my_fruit_list)
-st.dataframe(fruits_to_show)
-'''
