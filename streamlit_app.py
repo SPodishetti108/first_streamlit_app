36f7c2207331908dfeef70763d915708ab738508
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-10 03:01:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,27 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 import requests
 
-
+#st.title('My Parents New Healthy Diner')
 st.title('My Mom\'s New Healthy Diner')
-st.header('Breakfast Menu')
+st.header('Breakfast Favorites')
+st.text('🍞Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show  = my_fruit_list.loc[fruits_selected]
+# Display the table on the page.
+#st.dataframe(my_fruit_list)
+#st.dataframe(fruits_to_show)
+#st.title('My Mom\'s New Healthy Diner')
+st.header('Fruityvice Fruit Advice!')
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
