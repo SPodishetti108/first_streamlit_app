11cfcc2c6589ed0ff7b5c08da1f6652c42c40c4c
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-10 03:13:22 +0530

    Update streamlit_app.py
    err

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show  = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 #st.dataframe(my_fruit_list)
-#st.dataframe(fruits_to_show)
+st.dataframe(fruits_to_show)
 #st.title('My Mom\'s New Healthy Diner')
 st.header('Fruityvice Fruit Advice!')
 #import requests
