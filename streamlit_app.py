9265ca939143f40dbc91528f2401b3d93fa3ed5b
SPodishetti108 <135018900+SPodishetti108@users.noreply.github.com>
2023-08-10 02:55:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ import requests
 st.title('My Mom''s New Healthy Diner')
 st.header('Breakfast Menu')
 #import requests
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
